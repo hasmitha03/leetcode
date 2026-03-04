25121b79212cbdff6ecd3460c7da9bd0ff4faa91
Hasmitha M <hasmitharaj03@gmail.com>
2026-03-04 22:00:20 +0530

    Time: 4 ms (37.32%) | Memory: 12.3 MB (85.65%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProduct(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        max1 = 0
+        max2 = 0
+
+        while n > 0:
+            digit = n % 10
+                                                
+            if digit > max1:
+                max2 = max1  
+                max1 = digit
+            elif digit > max2:
+                max2 = digit
+            n //= 10
+        return max1 * max2
