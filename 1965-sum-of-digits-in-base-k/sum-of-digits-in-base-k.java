7ca888df89b8bc084c346e4495086502f06d65dd
Hasmitha Mahalakshmi Mantharapu <hasmitharaj03@gmail.com>
2026-03-15 21:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (52.80%) - LeetSync

---
+++ 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int sumBase(int n, int k) {
+       int ans = 0;
+       while (n > 0) {
+            int temp = n % k;
+            ans += temp;
+            n /= k;
+        }
+        return ans;
+    }
+}
